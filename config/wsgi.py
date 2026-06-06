0a4af27c12064e3dba34b55dd33dbdd66cefd8f4
Khizar Junaid <khizarxjunaid@gmail.com>
2026-06-06 15:16:13 +0500

    Update WSGI config
    Added project root directory to Python path in WSGI config.

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the project root directory to python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
